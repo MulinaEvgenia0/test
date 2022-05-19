698465c2bf09a62fc06a2836c07e84b585b27a3e
MulinaEvgenia0 <98643147+MulinaEvgenia0@users.noreply.github.com>
2022-05-19 16:11:31 +0500

    Delete client directory

--- client/src/api/Login.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import axios from 'axios';
-export const LoginFormApi = data => {
-    return axios.post('http://localhost:5000/api/users/auth/signin', data)
-}
--- client/src/api/Register.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import axios from 'axios';
-export const RegisterFormApi = async data => {
-    return axios.post('http://localhost:5000/api/users/auth/signup', data)
-}
